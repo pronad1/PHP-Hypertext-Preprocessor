de3ef39658bb618e48f21d5b3229f307c21dda74
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-10 01:01:40 +0600

    12/10/24

--- print.php
+++ print.php
@@ -1,7 +1,7 @@
 <?php
 // This is comment
-echo "I am Prosenjit Mondol.<br>";
-
+echo '<h2>'. "I am Prosenjit Mondol<br>" .'</h2>';
+print "I'm about to learn PHP!<br>";
 $sa=75586548956;
 echo "My secrate salary is $sa<br>";
 
@@ -10,4 +10,7 @@ $y=20;
 echo "Sum of two variable $x and $y is ",$x+$y;
 echo "<br>The data type and the value of $x is ";
 var_dump($x);
+$f="Mango";
+$t=$f;
+echo "<br>My favourite fruit is $t";
 ?>
