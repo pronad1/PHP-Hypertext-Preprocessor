adaeacab63a757a445aca52b9668d6c828e447e8
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-10 00:51:04 +0600

    12/10/24

--- print.php
+++ print.php
@@ -1,6 +1,13 @@
 <?php
+// This is comment
 echo "I am Prosenjit Mondol.<br>";
 
 $sa=75586548956;
-echo "My secrate salary is $sa";
+echo "My secrate salary is $sa<br>";
+
+$x=10;
+$y=20;
+echo "Sum of two variable $x and $y is ",$x+$y;
+echo "<br>The data type and the value of $x is ";
+var_dump($x);
 ?>
