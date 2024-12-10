fe245b7caffceb396c5332f87d69fc6da5fec812
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-10 14:16:06 +0600

    12/10/24

---
+++ create_table.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="myphp";
+
+$con=new mysqli($servername,$username,$password,$dbname);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_errno());
+}
+
+$sql="CREATE TABLE Accademic(
+id int(6) auto_increment primary key,
+nam varchar(30),
+mail varchar(20),
+dep varchar(20)
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+
+  $con->close();
+
+?>
