499d7d187e1243a599fc64c1cfc5b5eef7d96042
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-12-10 22:53:52 +0600

    12/10/24

---
+++ order.php
@@ -0,0 +1,25 @@
+<?php
+
+$sn="localhost";
+$un="root";
+$pa="";
+$db="myphp";
+
+$co=new mysqli($sn,$un,$pa,$db);
+if(!$co){
+    die("Connection failed: ".mysqli_connect_errno());
+}
+$sql="SELECT id,nam,dep FROM accademic order by nam";
+
+$res=$co->query($sql);
+
+if ($res->num_rows >0) {
+    while ($row=$res->fetch_assoc()) {
+        echo "id: ",$row["id"]," - Name: ",$row["nam"]," -Dept: ",$row["dep"],"<br>";
+    }
+}
+else {
+    echo "0 results: ";
+}
+$co->close();
+?>
