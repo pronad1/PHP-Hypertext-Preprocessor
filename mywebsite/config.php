945767b2989f508fd1eab4d59cc01ecdda168370
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-16 14:29:00 +0600

    2/16/25

--- mywebsite/config.php
+++ mywebsite/config.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = "";  
 $dbname = "student"; 
 
-$conn = new mysqli( $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
